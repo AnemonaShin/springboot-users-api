67170083dcf4109603d6f14311fb9481d4ced5f5
AnemonaShin <cramireza1997@gmail.com>
2026-05-10 22:41:21 -0400

    Changes on UserModel, Repository, Application.yml, .gitignore and VSCode configurations

--- src/main/java/cl/users/api/model/UserModel.java
+++ src/main/java/cl/users/api/model/UserModel.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.sql.Date;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,6 +14,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Table(name = "User", schema = "public")
 @Data
 @AllArgsConstructor
@@ -22,14 +24,12 @@ public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id", unique = true, nullable = false)
+    @Column(name = "user_id", unique = true, nullable = false, columnDefinition = "BIGINT")
     BigInteger id;
     @Column(nullable = false)
     String name;
     @Column(nullable = false)
     String lastName;
-    @Column(nullable = false)
-    String fullName;
     @Column(unique = true, nullable = false)
     String email;
     @Column(nullable = false)
---
+++ src/main/java/cl/users/api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package cl.users.api.repository;
+
+import java.math.BigInteger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import cl.users.api.model.UserModel;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, BigInteger> {
+
+}
